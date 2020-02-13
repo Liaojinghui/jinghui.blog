33c41e1317111712f97273a365008ff38012aeb9
Jinghui Liao <jinghui@wayne.edu>
2020-02-13 16:47:32 -0500

    change

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
+  title: 'Block Bloger', // Required
+  author: 'Jinghui Liao', // Required
+  description: 'A computer security PhD student who prefers blockchain and 6am sunlight.',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
+    website: 'jinghui.me',
+    github: 'https://github.com/Liaojinghui',
     twitter: 'https://twitter.com/ryanafitzgerald',
     linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
   },
