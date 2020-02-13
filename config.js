1bc1f76a79594eba2b93db014a28340bd3899655
Jinghui Liao <jinghui@wayne.edu>
2020-02-13 16:51:51 -0500

    change

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ module.exports = {
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'jinghui.me',
+    website: 'https://jinghui.me',
     github: 'https://github.com/Liaojinghui',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    twitter: 'https://twitter.com/vincent88033664',
+    linkedin: 'https://www.linkedin.com/in/jinghui-liao-2489b3144/',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://github.com/Liaojinghui/jinghui.blog',
 };
