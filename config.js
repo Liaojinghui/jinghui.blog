31ec964db409dc0a937398baa68a740fbbff32a4
Jinghui Liao <jinghui@wayne.edu>
2020-12-15 20:22:49 -0500

    change

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  title: 'Block Blogger', // Required
-  author: 'Jinghui Liao', // Required
-  description: 'A computer security PhD student who prefers blockchain and 6am sunlight and misses his girlfriend and family.',
-  primaryColor: '#3498db', // Required
-  showHeaderImage: true,
-  showShareButtons: true,
-  postsPerPage: 5, // Required
-  social: {
-    website: 'https://jinghui.me',
-    github: 'https://github.com/Liaojinghui',
-    twitter: 'https://twitter.com/vincent88033664',
-    linkedin: 'https://www.linkedin.com/in/jinghui-liao-2489b3144/',
-  },
-  pathPrefix: '/devblog',
-  siteUrl: 'https://github.com/Liaojinghui/jinghui.blog',
+    title: 'Block Blogger', // Required
+    author: 'Jinghui Liao', // Required
+    description: 'A computer security PhD student who prefers blockchain and 6am sunlight and misses his family.',
+    primaryColor: '#3498db', // Required
+    showHeaderImage: true,
+    showShareButtons: true,
+    postsPerPage: 5, // Required
+    social: {
+        website: 'https://jinghui.me',
+        github: 'https://github.com/Liaojinghui',
+        twitter: 'https://twitter.com/vincent88033664',
+        linkedin: 'https://www.linkedin.com/in/jinghui-liao-2489b3144/',
+    },
+    pathPrefix: '/devblog',
+    siteUrl: 'https://github.com/Liaojinghui/jinghui.blog',
 };
